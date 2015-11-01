55a5a6a32f9dde7278bbd1bff14656b956bf8dc5
anagaf <anagaf@gmail.com>
2015-11-01 14:41:58 +0300

    Updated UIAppearance

--- Freqhelper/Freqhelper/AppDelegate.swift
+++ Freqhelper/Freqhelper/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UILabel.appearance().textColor = UIColor.whiteColor()
+        UITextField.appearance().textColor = UIColor.whiteColor()
+
         return true
     }
 
